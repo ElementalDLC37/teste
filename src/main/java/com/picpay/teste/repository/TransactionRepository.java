3b86bb1ca445283e955a874429a0d902fe93c1ac
ElementalDLC37 <lucasantony815@gmail.com>
2024-07-06 21:40:43 -0300

    transaction repository

--- src/main/java/com/picpay/teste/repository/TransactionRepository.java
+++ src/main/java/com/picpay/teste/repository/TransactionRepository.java
@@ -1,5 +1,9 @@
 package com.picpay.teste.repository;
 
-public class TransactionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.picpay.teste.model.TransactionModel;
+
+public interface TransactionRepository extends JpaRepository<TransactionModel, Long> {
     
 }
